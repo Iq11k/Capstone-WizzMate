bf679f979cffbb2e5f1a283dc76acb4af7c1e428
Moch Rizqi <rizqia417@gmail.com>
2024-12-02 07:37:55 +0700

    improve HomePage UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        buildConfigField("String", "BASE_URL", "\"https://zj7336hw-5000.asse.devtunnels.ms/\"")
+        buildConfigField("String", "BASE_URL", "\"https://capstone-wizz-mate.vercel.app/\"")
         buildConfigField("String", "AUTH_WEB_CLIENT_ID", "\"145207385445-6iua2nbn88p4l4pjuf88s5ke9hfep36n.apps.googleusercontent.com\"")
 
     }
